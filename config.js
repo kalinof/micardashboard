e2826aa23ef005aab640c96d1a603cf41c979566
Digital Euro Association <florian.schreiber@digital-euro-association.de>
2025-09-25 17:52:11 +0200

    Add CASPs data integration and dashboard tab

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const DEFAULT_CSV_URL = 'https://docs.google.com/spreadsheets/d/1RfeiT68rH65izevXw_Upqdn0lXz-IGI83Zn3q0SBEbE/export?format=csv&id=1RfeiT68rH65izevXw_Upqdn0lXz-IGI83Zn3q0SBEbE&gid=0';
 const DEFAULT_DATE_URL = 'https://docs.google.com/spreadsheets/d/1RfeiT68rH65izevXw_Upqdn0lXz-IGI83Zn3q0SBEbE/export?format=csv&gid=353293525';
 const DEFAULT_NON_COMPLIANT_URL = 'https://docs.google.com/spreadsheets/d/1RfeiT68rH65izevXw_Upqdn0lXz-IGI83Zn3q0SBEbE/export?format=csv&gid=409089345';
+const DEFAULT_CASPS_URL = 'https://docs.google.com/spreadsheets/d/1RfeiT68rH65izevXw_Upqdn0lXz-IGI83Zn3q0SBEbE/export?format=csv&gid=1275732000';
 
 module.exports = {
     csvUrl: process.env.CSV_URL || DEFAULT_CSV_URL,
     dateUrl: process.env.DATE_URL || DEFAULT_DATE_URL,
-    nonCompliantUrl: process.env.NON_COMPLIANT_URL || DEFAULT_NON_COMPLIANT_URL
+    nonCompliantUrl: process.env.NON_COMPLIANT_URL || DEFAULT_NON_COMPLIANT_URL,
+    caspsUrl: process.env.CASPS_URL || DEFAULT_CASPS_URL
 };
 
